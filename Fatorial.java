5f2fab768481dc62a9eabed6a6893484635bec07
Breno Jesus <breno.jesus33@hotmail.com>
2025-05-17 00:08:26 +0000

    refactor: refatoração do código para facilitar os testes

--- Fatorial.java
+++ Fatorial.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+public class Fatorial { 
 
-public class Fatorial {
+    public static long calcularFatorial(int numero) {
+        long fatorial = 1;
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.print("Digite um número inteiro positivo: ");
         int numero = scanner.nextInt();
 
         if (numero < 0) {
             System.out.println("Número inválido. Insira um número positivo.");
         } else {
-            long fatorial = 1;
-            for (int i = 1; i <= numero; i++) {
-                fatorial *= i;
-            }
-            System.out.println("Fatorial de " + numero + " é: " + fatorial);
+            System.out.println("Fatorial de " + numero + " é: " + calcularFatorial(numero));
         }
 
         scanner.close();
